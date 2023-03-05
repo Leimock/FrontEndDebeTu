ad84de7c61fd2234062725fdf165cc7488a70950
Leimock <miguelestevezpardo@gmail.com>
2023-03-05 20:31:40 +0100

    little fix

--- src/pages/UserAdd.jsx
+++ src/pages/UserAdd.jsx
@@ -9,9 +9,9 @@ const SignUp = () => {
         <>
             <h1 className="flex flex-col mb-6">Registrarse</h1>
             <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-                <input type="text" name="name" value={user.name} onChange={handleChange}/>
-                <input type="email  " name="email" value={user.email} onChange={handleChange}/>
-                <input type="password" name="password" value={user.password} onChange={handleChange}/>
+                <input type="text" name="name" value={user.name} onChange={handleChange} placeholder="Nombre"/>
+                <input type="email  " name="email" value={user.email} onChange={handleChange} placeholder="Correo"/>
+                <input type="password" name="password" value={user.password} onChange={handleChange} placeholder="Contraseña"/>
                 <button type="submit" className="border-red-200">Enviar</button>
             </form>
             {error && <p style={{color: 'red'}}>{error}</p>}
